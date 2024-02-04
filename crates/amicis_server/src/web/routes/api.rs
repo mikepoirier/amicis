a150ddd627b36c9594768fc08845de68820866b1
Gabriel Sullice <gabriel@sullice.com>
2024-02-04 15:50:16 +0100

    scaffold api document

--- crates/amicis_server/src/web/routes/api.rs
+++ crates/amicis_server/src/web/routes/api.rs
@@ -1,10 +1,14 @@
+use std::collections::HashMap;
 use axum::{response::IntoResponse, routing::get, Json, Router};
 use serde::Serialize;
+use crate::web::routes::ResourceType::{CorrespondenceOverview, CorrespondenceTeaser, TransmissionTeaser};
 
-use super::{health_status, HealthStatus};
+use super::{Document, health_status, HealthStatus, Link, Resource};
 
 pub fn routes() -> Router {
-    Router::new().route("/health", get(get_health))
+    Router::new()
+        .route("/health", get(get_health))
+        .route("/correspondences", get(get_correspondences))
 }
 
 #[derive(Debug, Serialize)]
@@ -15,4 +19,73 @@ struct HealthResponse {
 async fn get_health() -> impl IntoResponse {
     let status = health_status().await;
     Json(HealthResponse { status })
+}
+
+async fn get_correspondences() -> impl IntoResponse {
+    let mut correspondences = HashMap::new();
+    correspondences.insert(
+        "correspondences".to_string(),
+        vec![
+            Link{
+                rel: "item".to_string(),
+                href: "/correspondences/1".to_string(),
+                target_title: "Keeping in touch".to_string(),
+                target_type: None,
+            },
+            Link{
+                rel: "item".to_string(),
+                href: "/correspondences/2".to_string(),
+                target_title: "Amicis development".to_string(),
+                target_type: None,
+            }],
+    );
+    let mut transmissions = HashMap::new();
+    transmissions.insert(
+        "transmissions".to_string(),
+        vec![
+            Link{
+                rel: "item".to_string(),
+                href: "/transmission/d7c80354-a8a9-4a02-ac3d-da79f9b64150".to_string(),
+                target_title: "Trying this out".to_string(),
+                target_type: None,
+            },
+    ],
+    );
+    Json(Document{
+        data: Resource{
+            kind: CorrespondenceOverview,
+            uri: "/correspondences".to_string(),
+            attributes: HashMap::from([
+                ("title".to_string(), "Correspondences".to_string())
+            ]),
+            relationships: Some(correspondences),
+        },
+        included: Some(vec![
+            Resource{
+                kind: CorrespondenceTeaser,
+                uri: "/correspondences/1".to_string(),
+                attributes: HashMap::from([
+                    ("title".to_string(), "Keeping in touch".to_string()),
+                ]),
+                relationships: Some(transmissions),
+            },
+            Resource{
+                kind: CorrespondenceTeaser,
+                uri: "/correspondences/2".to_string(),
+                attributes: HashMap::from([
+                    ("title".to_string(), "Amicis development".to_string()),
+                ]),
+                relationships: None,
+            },
+            Resource{
+                kind: TransmissionTeaser,
+                uri: "/transmission/d7c80354-a8a9-4a02-ac3d-da79f9b64150".to_string(),
+                attributes: HashMap::from([
+                    ("subject".to_string(), "Trying this out".to_string()),
+                    ("body".to_string(), "Wazzup, bruh?".to_string()),
+                ]),
+                relationships: None,
+            },
+        ]),
+    })
 }
